d80c1e099eee35d8f3104e0ee81d15fd03b76426
Ryan Moore <ryan.moore@tagglogistics.com>
2019-12-14 02:03:22 -0600

    getting staticfiles in place

---
+++ frontend/src/components/BaseLine.vue
@@ -0,0 +1,78 @@
+<template>
+  <v-app id="inspire">
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+    >
+      <v-list dense>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-folder-multiple-image</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Slide List</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-telescope</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Current Slide</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-pencil</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Review Classifications</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar
+      app
+      color="indigo"
+      dark
+    >
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <v-toolbar-title>Digital Slide Classification Demo</v-toolbar-title>
+    </v-app-bar>
+
+    <v-content>
+      <v-container
+        class="fill-height"
+        fluid
+      >
+        <v-row
+          align="center"
+          justify="center"
+        >
+          <v-col class="text-center">
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-content>
+    <v-footer
+      color="indigo"
+      app
+    >
+      <span class="white--text">&copy; 2019</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+  export default {
+		name: 'BaseLine',
+
+    props: {
+      source: String,
+    },
+    data: () => ({
+      drawer: null,
+    }),
+  }
+</script>
